c1820d5449d6b865a3416eb95a919d84c97df3fa
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-04-10 09:43:46 +0200

    npm run weather displays the weather in San Fransisco

--- weather.js
+++ weather.js
@@ -1,1 +1,12 @@
-console.log("coucou")
+const weather = require('weather-js');
+const expect  = import('chai').then(e => {return e.expect}).then(
+    weather.find({search: 'San Fransisco, 94102', degreeType: 'C'}, function(err, result) {
+      if(err) console.log(err);
+      console.log(JSON.stringify(result, null, 2));
+    })
+);
+console.log(expect)
+// Options:
+// search:     location name or zipcode
+// degreeType: F or C
+ 
